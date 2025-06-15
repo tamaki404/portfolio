9145fc6b118803380ae6f3366c01159331f36cf6
tamaki404 <nicole.tumpag2003@gmail.com>
2025-06-15 08:58:33 +0800

    Initial commit for portfolio project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nicole - Profile</title>
+   
+    <meta name="description" content="Nicole's profile page showcasing user details.">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="icon" href="">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Anton&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Savate:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+   
+</head>
+<body>
+
+<main class="mainCon">
+    <section class="introSec">
+         <div class="directCon">
+            <button class="directButt" ><p style="font-weight: bold">About</p></button>
+            <button class="directButt"><p>Projects</p></button>
+            <button class="directButt"><p>Resume</p></button>
+            <button class="directButt"><p>Gallery</p></button>
+            <button class="directButt"><p>Contact</p></button>
+            
+         </div>
+
+          <div class="twoCol">
+              <div class="col1">
+                <div class="introText">
+                  <h1 class="title">Heya!</h1>
+                  <h1 class="name" >I'm Nicole</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fug
+                  </p>
+                  <div class="socials">
+                    <div class="socialButt">
+                      <a href="#">
+                    </div>  
+                        <div class="socialButt">
+                      <a href="#">
+                    </div>  
+                        <div class="socialButt">
+                      <a href="#">
+                    </div>  
+                                            <div class="socialButt">
+                      <a href="#">
+                    </div>  
+                    
+
+                  </div>
+               
+
+                  
+                </div>
+
+        
+              </div>
+              <div class="col2">
+                      none yet ...
+              </div>
+
+            </div>
+             
+    </section>
+    <section class="detailsSec">
+     
+
+    </section>
+
+</main>
+
+</body>
+</html>
